1c05489076fb0dcabdc078e10aeb91b62359032e
Tasuku Watanabe <tasuku.watanabe.325@gmail.com>
2022-03-12 23:59:30 +0900

    fetch listings from firebase

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,6 +1,6 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { getAuth, updateProfile } from 'firebase/auth';
-import { useNavigate, Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { db } from '../firebase.config';
 import { updateDoc, doc } from 'firebase/firestore';
 import { toast } from 'react-toastify';
@@ -37,7 +37,7 @@ function Profile() {
           name,
         });
       }
-    } catch (error){
+    } catch (error) {
       toast.error('Could not update profile details');
     }
   };
